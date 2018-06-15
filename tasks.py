7249d56b81ef79ac2d730fa067b79dbe7a441a4a
JesusMtnez <jesusmartinez93@gmail.com>
2018-06-15 13:20:23 +0200

    Use invoke instead of fabfile
    Fabfiles 2.x version now supports Python 3, but to handle local
    command execution, it uses invoke library. Since the purpose of this
    tools is just to manage the blog locally, it's easier to use just
    invoke instead fabfiles.

--- tasks.py
+++ tasks.py
@@ -1,40 +1,43 @@
-from fabric.api import *
-import fabric.contrib.project as project
+from invoke import task
+from pelican.server import ComplexHTTPRequestHandler
 import os
 import shutil
 import sys
 import socketserver
 
-from pelican.server import ComplexHTTPRequestHandler
-
 # Local path configuration (can be absolute or relative to fabfile)
-env.deploy_path = 'public'
-DEPLOY_PATH = env.deploy_path
+DEPLOY_PATH = 'public'
 
 # Port for `serve`
 PORT = 8000
 
-def clean():
+@task
+def clean(c):
     """Remove generated files"""
     if os.path.isdir(DEPLOY_PATH):
         shutil.rmtree(DEPLOY_PATH)
         os.makedirs(DEPLOY_PATH)
-
-def build():
+@task
+def build(c):
     """Build local version of site"""
-    local('pelican -s pelicanconf.py')
+    # TODO Download pelican-themes if needed
+    # TODO Download Flex theme if needed
+    c.run('pelican -s pelicanconf.py')
 
-def rebuild():
+@task
+def rebuild(c):
     """`build` with the delete switch"""
-    local('pelican -d -s pelicanconf.py')
+    c.run('pelican -d -s pelicanconf.py')
 
-def regenerate():
+@task
+def regenerate(c):
     """Automatically regenerate site upon file modification"""
-    local('pelican -r -s pelicanconf.py')
+    c.run('pelican -r -s pelicanconf.py')
 
-def serve():
+@task
+def serve(c):
     """Serve site at http://localhost:8000/"""
-    os.chdir(env.deploy_path)
+    os.chdir(DEPLOY_PATH)
 
     class AddressReuseTCPServer(socketserver.TCPServer):
         allow_reuse_address = True
@@ -44,11 +47,13 @@ def serve():
     sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
     server.serve_forever()
 
-def reserve():
+@task
+def reserve(c):
     """`build`, then `serve`"""
-    build()
-    serve()
+    build(c)
+    serve(c)
 
-def preview():
+@task
+def preview(c):
     """Build production version of site"""
-    local('pelican -s publishconf.py')
+    c.run('pelican -s publishconf.py')
