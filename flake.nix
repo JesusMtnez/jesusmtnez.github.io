4d258cff1c416882947e2370c9d40df6541cac73
JesusMtnez <jesusmartinez93@gmail.com>
2023-05-25 07:42:56 +0200

    replace flake-utils with flake-parts

--- flake.nix
+++ flake.nix
@@ -1,46 +1,55 @@
 {
-  description = "JesusMtnez's blog";
+  description = "jesusmtnez.es flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
     theme = {
       url = "github:luizdepra/hugo-coder";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, theme }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        themeName = ((builtins.fromTOML (builtins.readFile "${theme}/theme.toml")).name);
-      in
-      {
+  outputs = inputs@{ self, nixpkgs, flake-parts, theme }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        packages.website = pkgs.stdenv.mkDerivation rec {
-          pname = "jesusmtnez-website";
-          version = (builtins.substring 0 8 self.lastModifiedDate);
-          src = ./.;
-          nativeBuildInputs = [ pkgs.hugo ];
-          configurePhase = ''
-            mkdir -p "themes/${themeName}"
-            cp -r ${theme}/* "themes/${themeName}"
-          '';
-          buildPhase = "hugo --destination $out";
-          dontInstal = true;
+      perSystem = { pkgs, ... }:
+        let
+          themeName = ((builtins.fromTOML (builtins.readFile "${theme}/theme.toml")).name);
+        in
+        {
+
+          packages.default = pkgs.stdenv.mkDerivation rec {
+            pname = "jesusmtnez-website";
+            version = (builtins.substring 0 8 self.lastModifiedDate);
+            src = ./.;
+            nativeBuildInputs = [ pkgs.hugo ];
+            configurePhase = ''
+              mkdir -p "themes/${themeName}"
+              cp -r ${theme}/* "themes/${themeName}"
+            '';
+            buildPhase = "hugo --destination $out";
+            dontInstal = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "website-shell";
+            packages = [ pkgs.hugo ];
+            shellHook = ''
+              mkdir -p "themes"
+              [[ ! -d themes/${themeName} ]] && ln -sn ${theme}/* "themes/${themeName}"
+            '';
+          };
         };
-
-        defaultPackage = self.packages.${system}.website;
-
-        devShell = pkgs.mkShell {
-          name = "website-shell";
-          packages = [ pkgs.hugo ];
-          shellHook = ''
-            mkdir -p "themes"
-            [[ ! -d themes/${themeName} ]] && ln -sn ${theme}/* "themes/${themeName}"
-          '';
-        };
-      }
-    );
+    };
 }
